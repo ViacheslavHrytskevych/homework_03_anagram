07f09a91831c91b32c62a86dacd170b0a87199a8
Viacheslav Hrytskevych <hrytskevych.v@gmail.com>
2023-03-28 17:58:37 +0300

    solution

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine();
+        String t = scanner.nextLine();
+
+        System.out.println(isAnagram(s, t));
+
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++)
+            alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++)
+            alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet)
+            if (i != 0)
+                return false;
+        return true;
+    }
 }
